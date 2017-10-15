1353b4833bf3dccf4d3e2ef1b4eee34309e1cff6
kumin <273423480@qq.com>
2017-10-15 23:13:59 +0800

    add test code

---
+++ main.js
@@ -0,0 +1,15 @@
+﻿let APP_ID = 'E1tdtMXPl1dhn36xlLxVYJMk-gzGzoHsz';
+let APP_KEY = 'yF2RQ8kWLsAWDijlVYgDMPt7';
+
+var AV = require('leancloud-storage');
+
+AV.init({
+	appId: APP_ID,
+	appKey: APP_KEY
+});
+console.log(v2Domain);
+
+// var exec = require('child_process').exec;
+// exec('ping {{v2Domain}}',function(err,stdout,stderr){
+//     console.log(stdout);
+// });
